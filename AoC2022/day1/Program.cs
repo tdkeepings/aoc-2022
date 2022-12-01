1954842c46a2895504c3d33bf9bfec299b30c7f1
Tom Keepings <tom.keepings@prodigi.uk>
2022-12-01 11:16:01 +0000

    first day done

---
+++ AoC2022/day1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+var elfCalories = new List<int>();
+var currentCalorie = 0;
+foreach (var line in File.ReadLines("input.txt")){
+    
+    if (string.IsNullOrEmpty(line))
+    {
+        elfCalories.Add(currentCalorie);
+        currentCalorie = 0;
+        continue;
+    }
+
+    currentCalorie += Convert.ToInt32(line);
+}
+
+// Console.WriteLine(elfCalories.MaxBy(x => x)); // Part 1
+Console.WriteLine(elfCalories.OrderByDescending(x => x).Take(3).Sum()); // Part 2
