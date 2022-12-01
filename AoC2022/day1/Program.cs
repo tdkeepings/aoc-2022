252659c2bb6c1c5da85ec1a6787ce68aa06864b9
Tom Keepings <tom.keepings@prodigi.uk>
2022-12-01 11:24:39 +0000

    added some extra solutions that use less memory

--- AoC2022/day1/Program.cs
+++ AoC2022/day1/Program.cs
@@ -15,3 +15,54 @@ foreach (var line in File.ReadLines("input.txt")){
 
 // Console.WriteLine(elfCalories.MaxBy(x => x)); // Part 1
 Console.WriteLine(elfCalories.OrderByDescending(x => x).Take(3).Sum()); // Part 2
+
+// ----------------------------
+// Solution 2 part 1 - memory efficient
+// ----------------------------
+// var highest = 0;
+// var currentCalorie = 0;
+// foreach (var line in File.ReadLines("input.txt")){
+//     
+//     if (string.IsNullOrEmpty(line))
+//     {
+//         if (currentCalorie > highest)
+//         {
+//             highest = currentCalorie;
+//         } 
+//         currentCalorie = 0;
+//         continue;
+//     }
+//
+//     currentCalorie += Convert.ToInt32(line);
+// }
+//
+// Console.WriteLine(highest);
+
+
+
+// ----------------------------
+// Solution 2 part 2 - memory efficient
+// ----------------------------
+// var topN = 3;
+// var highest = new int[topN];
+// var currentCalorie = 0;
+// foreach (var line in File.ReadLines("input.txt")){
+//
+//     if (string.IsNullOrEmpty(line))
+//     {
+//         for (int i = 0; i < topN; i++)
+//         {
+//             if (currentCalorie > highest[i])
+//             {
+//                 highest[i] = currentCalorie;
+//                 break;                
+//             }
+//         }
+//         
+//         currentCalorie = 0;
+//         continue;
+//     }
+//     currentCalorie += Convert.ToInt32(line);
+// }
+//
+// Console.WriteLine(highest.Sum());
